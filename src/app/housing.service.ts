db6fb8a8055e395eef255e948e4fbb02f2175022
HidriSana <115165735+HidriSana@users.noreply.github.com>
2024-01-12 16:13:28 +0100

    Added a form in details page

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -5,6 +5,23 @@ import { HousingLocation } from './housinglocation';
   providedIn: 'root',
 })
 export class HousingService {
+  constructor() {}
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(
+      (housingLocation) => housingLocation.id === id
+    );
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`
+    );
+  }
+
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
 
   housingLocationList: HousingLocation[] = [
@@ -109,15 +126,4 @@ export class HousingService {
       laundry: true,
     },
   ];
-
-  constructor() {}
-  getAllHousingLocations(): HousingLocation[] {
-    return this.housingLocationList;
-  }
-
-  getHousingLocationById(id: number): HousingLocation | undefined {
-    return this.housingLocationList.find(
-      (housingLocation) => housingLocation.id === id
-    );
-  }
 }
